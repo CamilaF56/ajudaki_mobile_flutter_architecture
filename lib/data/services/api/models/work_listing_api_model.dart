ba159af1851c6be9ed158746fc8ca3f038873b3b
BouncyBox <camilafontes56@gmail.com>
2025-12-14 19:58:06 -0300

    Atualiza WorkListingApiModel

--- lib/data/services/api/models/work_listing_api_model.dart
+++ lib/data/services/api/models/work_listing_api_model.dart
@@ -2,19 +2,19 @@ import 'api_model.dart';
 
 class WorkListingApiModel extends ApiModel {
   final int workTypeId;
-  final int creatorId;
+  final int creatorPersonId;
   final String title;
   final String description;
-  final String picture;
+  final String pictureUrl;
   final int estimatedPrice;
 
   const WorkListingApiModel({
     required super.id,
     required this.workTypeId,
-    required this.creatorId,
+    required this.creatorPersonId,
     required this.title,
     required this.description,
-    required this.picture,
+    required this.pictureUrl,
     required this.estimatedPrice
   });
 
@@ -22,10 +22,10 @@ class WorkListingApiModel extends ApiModel {
     return WorkListingApiModel(
       id: json['id'] as int,
       workTypeId: json['workTypeId'] as int,
-      creatorId: json['creatorId'] as int,
+      creatorPersonId: json['creatorPersonId'] as int,
       title: json['title'] as String,
       description: json['description'] as String,
-      picture: json['picture'] as String,
+      pictureUrl: json['pictureUrl'] as String,
       estimatedPrice: json['estimatedPrice'] as int,
     );
   }
