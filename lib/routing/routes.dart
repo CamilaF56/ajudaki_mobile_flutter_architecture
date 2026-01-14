25941c20bc5e9ae46d0c5ae70926b7cc8e19b755
BouncyBox <camilafontes56@gmail.com>
2026-01-14 01:21:08 -0300

    Revert "Merge branch 'staging' of https://github.com/CamilaF56/ajudaki_mobile_flutter_architecture into staging"
    This reverts commit 9becd9ef0a8d935a91bc161bf226b48c2795c00f, reversing
    changes made to d68e28b485d954a9e2fd9229d48bb4bfb4d430c9.

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -2,4 +2,8 @@ abstract final class Routes {
   static const home = search;
 
   static const search = '/';
+
+  static const detailsRelative = 'details';
+  static const details = '/$detailsRelative';
+  static String detailsWithId(int id) => '/$details/$id';
 }
